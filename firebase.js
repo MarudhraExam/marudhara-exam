28015faddfbc30de6ede96d2a7d219d64a85455a
MarudhraExam <jmdseller2025@gmail.com>
2026-06-17 11:25:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+// Firebase Config
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyDHe87UG-QGyZKxh7RI8t51q0GgppVd_YA",
+authDomain: "marudhara-exam.firebaseapp.com",
+projectId: "marudhara-exam",
+storageBucket: "marudhara-exam.firebasestorage.app",
+messagingSenderId: "680152404373",
+appId: "1:680152404373:web:23515d1d06c44dbd33669c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
