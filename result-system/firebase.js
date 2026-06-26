c19b559bb81800f43250f010f515c65ce2da779a
MarudhraExam <jmdseller2025@gmail.com>
2026-06-26 23:46:53 +0530

    Update firebase.js

--- result-system/firebase.js
+++ result-system/firebase.js
@@ -1,1 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  orderBy,
+  limit,
+  writeBatch,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDHe87UG-QGyZKxh7RI8t51q0GgppVd_YA",
+
+  authDomain: "marudhara-exam.firebaseapp.com",
+
+  projectId: "marudhara-exam",
+
+  storageBucket: "marudhara-exam.firebasestorage.app",
+
+  messagingSenderId: "680152404373",
+
+  appId: "1:680152404373:web:23515d1d06c44dbd33669c"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  orderBy,
+  limit,
+  writeBatch,
+  serverTimestamp
+};
