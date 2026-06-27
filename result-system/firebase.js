5e9a2b3c4a0edebdc05d98d1d5b8f7f4bf31e701
MarudhraExam <jmdseller2025@gmail.com>
2026-06-27 18:15:34 +0530

    Update firebase.js

--- result-system/firebase.js
+++ result-system/firebase.js
@@ -41,6 +41,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Export instances and modular query handlers to structural scripts
+window.db = db;
 export {
     db,
     collection,
