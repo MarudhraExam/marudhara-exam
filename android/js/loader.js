65f25d86bff1543892eeb2d40caf13fac8e64c3d
MarudhraExam <jmdseller2025@gmail.com>
2026-07-19 19:17:47 +0530

    Update loader.js

--- android/js/loader.js
+++ android/js/loader.js
@@ -1,1 +1,18 @@
+(function () {
+    'use strict';
 
+    // Detect Android App
+    const isAndroidApp =
+        navigator.userAgent.includes("MarudharaExamAndroidApp");
+
+    if (!isAndroidApp) {
+        return;
+    }
+
+    console.log("Marudhara Exam Android Mode Enabled");
+
+    // Android class
+    document.documentElement.classList.add("android-app");
+    document.body.classList.add("android-app");
+
+})();
